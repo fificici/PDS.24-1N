5481fad296226264469a46263a3ba4eff4947a2b
fificici <114841536+fificici@users.noreply.github.com>
2024-03-25 20:29:15 -0300

    Add files via upload

---
+++ atividade-pds.js
@@ -0,0 +1,72 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+var numero1 = 3
+var numero2 = 4 
+var resultado
+//----------------------------------------
+resultado = numero1 == numero2
+
+print(numero1, '=', numero2, '?', resultado)
+//----------------------------------------
+resultado = numero1 != numero2
+
+print(numero1, '!=', numero2, '?', resultado)
+//----------------------------------------
+resultado = numero1 > numero2
+
+print(numero1, '>', numero2, '?', resultado)
+//----------------------------------------
+resultado = numero1 < numero2
+
+print(numero1, '<', numero2, '?', resultado)
+//----------------------------------------
+resultado = numero1 >= numero2
+
+print(numero1, '>=', numero2, '?', resultado)
+//----------------------------------------
+resultado = numero1 <= numero2
+
+print(numero1, '<=', numero2, '?', resultado)
+//----------------------------------------
+var a1 = true && true
+print(a1)
+//----------------------------------------
+var a2 = true && false
+print(a2)
+//----------------------------------------
+var a3 = false && true
+print(a3)
+//----------------------------------------
+var a4 = false && 3 == 4 
+print(a4)
+//----------------------------------------
+var a5 = 'gato' && 'cão'
+print(a5)
+//----------------------------------------
+var a6 = false && 'gato'
+print(a6)
+//----------------------------------------
+var a7 = 'gato' && false
+print(a7)
+//----------------------------------------
+var alfabeto = 'alfa'
+alfabeto += 'beto'
+
+print(alfabeto)
+//----------------------------------------
+var idade = 15
+var status = idade >= 18?'adulto':'menor de idade'
+
+print(status)
+//----------------------------------------
+var alfa = 'alfa'
+var beto = 'beto'
+var resultado = 'alfa' + 'beto'
+
+print(alfabeto)
+
